e79b9b4a1ef977c10afbfcf2e82a77918b2d70b6
andrei <andrei.ionescu49@yahoo.com>
2020-06-16 18:12:04 +0200

    hope this form works

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -10,6 +10,10 @@ export default {
     },
     methods: {
       /* TODO */
+      sendContact: function() {
+        console.log("This is where your data will be.");
+      }
+
     },
 
     template:
@@ -19,12 +23,12 @@ export default {
         <div v-for="item in data" class="card m-2">
             <div class="card-body">
                 <h3 class="card-title">{{item.title}}</h3>
-                <form v-for="input in item.inputs" class="p-3 bg">
+                <form id="contact-form" method="POST" action="mailto:andrei.ionescu49@yahoo.com" v-for="input in item.inputs" class="p-3 bg">
                     <label>{{input.label}}</label>
                     <textarea class="form-control" v-if="input.type=='textarea'"></textarea>
                     <div v-if="input.type=='textarea'" class="text-right">
                       <br><br>
-                      <button class="btn btn-primary">Send message</button>
+                      <input type="submit" value="Send">
                     </div>
                     <input v-if="input.type!=='textarea'"  placeholder="" class="form-control">
                 </form>
